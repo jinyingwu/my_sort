2eff81ab2395819ee8f35c480ab8215e44866e7d
jinying wu <jinyingwu@jinyings-MacBook-Air.local>
2018-08-26 00:46:54 -0700

    finish merge_sort

--- merge_sort/merge_sort.rb
+++ merge_sort/merge_sort.rb
@@ -1,7 +1,3 @@
-require "byebug"
-# arr = [8, 2, 3, 5, 1, 9, 9, 7, 5, 0]
-arr = [8, 2, 3]
-
 def merge_sort(arr)
   return arr if arr.length == 1
   return nil if arr.length == []
@@ -11,27 +7,22 @@ def merge_sort(arr)
   left_side = arr[0...mid]
   right_side = arr[mid..-1]
 
-  # byebug
-
   result += merge(merge_sort(left_side), merge_sort(right_side))
-
 end
 
 def merge(left_arr, right_arr)
-  # p left_arr if left_arr.length > 1
-  # p right_arr if right_arr.length > 1
-
   result = []
-  # byebug
+
   while left_arr != [] && right_arr != []
     if left_arr.first < right_arr.first
-      result << left_arr.pop
+      result << left_arr.shift
     else
-      result << right_arr.pop
+      result << right_arr.shift
     end
   end
 
-  result += right_arr + left_arr
+  result += left_arr + right_arr
 end
 
-p merge_sort(arr)
+arr = [8, 2, 3, 5, 1, 9, 9, 7, 5, 0]
+p merge_sort(arr) #=> [0, 1, 2, 3, 5, 5, 7, 8, 9, 9]
